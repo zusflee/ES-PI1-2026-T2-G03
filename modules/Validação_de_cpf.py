1a99fe0336a464614be758f1f414f6c9aa02beb0
gabrieljorgee8 <gabriel.aj@puccampinas.edu.br>
2026-04-21 16:38:10 -0300

    Validação de titulo

---
+++ modules/Validação_de_cpf.py
@@ -0,0 +1,38 @@
+def validação_de_cpf(cpf):
+    cpf = ''.join(filter(str.isdigit, cpf))#Vai limpar os pontos e traços contidos no cpf
+
+    if len(cpf) !=11: #Verificação para ver se tem os 11 digistos necessarios
+        return False 
+    if cpf == cpf[0] *11: #Verificação para ver se os numeros não são iguais
+        return False
+    
+    soma= 0 #primeiro digito verificador
+    for i in range(9):
+       soma+= int(cpf[i])*(10-i)
+    resto= soma % 11
+    if resto <2:
+        digito1 = 0
+    else:
+        digito1= 11 - resto
+    if digito1 != int(cpf[9]):
+      return False 
+    
+    soma=0
+    for i in range(10):
+       soma+= int(cpf[i])*(11-i)
+    resto= soma % 11
+    if resto <2:
+        digito2 = 0
+    else:
+       digito2= 11 - resto
+    if digito2 != int(cpf[10]):
+       return False
+       
+
+    return True
+CPF= input("Digite seu CPF: ") #Soalicita o cpf
+if validação_de_cpf(CPF):
+   print("CPF Valido")
+else:
+   print("CPF INVALIDO")
+    
