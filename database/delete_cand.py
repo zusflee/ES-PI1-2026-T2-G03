ed9986d02fe5c41e69f7e3007c8f58c9a85c8a09
Leonardo Souza <leonardo.as7@puccampinas.edu.br>
2026-04-23 22:18:41 -0300

    deletar candidato diretamente pelo visual studio

---
+++ database/delete_cand.py
@@ -0,0 +1,22 @@
+def deletar_candidato(conexao, cursor, numero_candidato):
+    try:
+        confirmacao = input(f"Tem certeza que deseja deletar o candidato nº {numero_candidato}? (s/n): ")
+
+        if confirmacao.lower() != "s":   # lower --> Converte todos os caracteres alfabéticos maiúsculos em uma string para minúscula. 
+            print("Operação cancelada.")
+            return  # Sai da função sem fazer nada
+
+        sql = "DELETE FROM candidatos WHERE numero = %s"
+        valor = (numero_candidato,)  # Vírgula obrigatória — tupla com 1 elemento
+
+        cursor.execute(sql, valor)
+        conexao.commit()
+
+        if cursor.rowcount > 0:
+            print(f"Candidato nº {numero_candidato} deletado com sucesso!")
+        else:
+            print(f"Nenhum candidato encontrado com o número {numero_candidato}.")
+
+    except mysql.connector.Error as erro:
+            conexao.rollback()
+            print(f"Erro ao deletar candidato: {erro}")
