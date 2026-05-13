9dae5f9bb6ef9b0b851c16fb47335ba553b37052
leleo <leonardo.as7@puccampinas.edu.br>
2026-05-12 22:28:22 -0300

    correcao abertura_urna

--- modules/abertura_urna.py
+++ modules/abertura_urna.py
@@ -27,35 +27,35 @@ def zerézima(cursor, conexao):
 
 def abertura_urna(cursor, conexao):
     print("\n--- ABERTURA DO SISTEMA DE VOTAÇÃO ---")
-    titulo= input("Digite seu titulo de eleitor: ")#pede o titulo
-    cmc_cpf= input("Digite os 4 primeiros digitos do CPF: ")#pede os 4 primeiros digitos do cpf
-    chave_acesso= input("Digite sua chave de acesso: ")#pede a chave de acesso
-    
-    cursor.execute("SELECT * FROM eleitores WHERE titulo = %s", (titulo,) )#vai buscar no bd o eleitor pelo titulo
-    eleitor = cursor.fetchone()
 
-    if not eleitor: #vai verificar se o eleitor existe
-        print("Dados incorretos. A validação falhou!")
-        return False
-    
-    if eleitor[2] != titulo: #vai verificar se o titulo que consta no banco esta igual ao digitado
-        print("Dados incorretos. A validação falhou!")
-        return False
-    
-    if eleitor[3][:4] != cmc_cpf: #vai verificar se os 4 primeiros digistos do cpf são iguais tanto no painel quanto no bd
-        print("Dados incorretos. A validação falhou!")
+    # Loop do título
+    eleitor = None
+    while eleitor is None:
+        titulo = input("Digite seu título eleitoral: ")
+        cursor.execute("SELECT * FROM eleitores WHERE titulo = %s", (titulo,))
+        eleitor = cursor.fetchone()
+        if eleitor is None:
+            print("Título não encontrado. Tente novamente.")
+
+    # Loop dos 4 primeiros dígitos do CPF
+    cmc_cpf = ""
+    while eleitor[3][:4] != cmc_cpf:
+        cmc_cpf = input("Digite os 4 primeiros dígitos do CPF: ")
+        if eleitor[3][:4] != cmc_cpf:
+            print("CPF incorreto. Tente novamente.")
+
+    # Loop da chave de acesso
+    chave_acesso = ""
+    while eleitor[4] != chave_acesso:
+        chave_acesso = input("Digite sua chave de acesso: ")
+        if eleitor[4] != chave_acesso:
+            print("Chave de acesso incorreta. Tente novamente.")
+
+    # Verifica se é mesário
+    if not eleitor[5]:
+        print("Erro. Este eleitor não possui perfil de mesário!")
         return False
 
-    if eleitor[4] !=chave_acesso: #vai verificar se a chave de acesso corresponde em ambos os lugares
-        print("Dados incorretos. A validação falhou!")
-        return False
-    
-    if not eleitor[5]: #vai verificar se tem registro como mesario
-        print("Erro. Este eleitor não possui perfil de mesario!")
-        return False
-     
     print(f"Bem vindo, {eleitor[1]}! Identidade validada com sucesso!")
-
     zerézima(cursor, conexao)
     return True
-
